99a561ca26b9d0813674c6cf0a93b06f9790e95b
Erik Strijk <edstrijk@st.noorderpoort.nl>
2021-09-13 14:56:14 +0200

    opdracht 1

---
+++ opdracht01.php
@@ -0,0 +1,13 @@
+<?php
+ $tekst = 'Dit is nog een keer de tekst van opdracht 1';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Opdracht 1</title>
+</head>
+<body>
+    <p> <?php echo $tekst ?> </p>
+</body>
+</html>
